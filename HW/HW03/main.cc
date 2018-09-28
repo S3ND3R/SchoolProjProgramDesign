b7a8fa2bfe5a209cff03d17e36b95c1efc9d5148
Warren Weber <weber767@umn.edu>
2018-09-28 09:55:56 -0500

    change(HW03): Added variable for loop control main.cc

--- HW/HW03/main.cc
+++ HW/HW03/main.cc
@@ -43,7 +43,8 @@ void doInterestingThing(int yr) {
   }
 
   // now show the dates accumulated in reverse order
-  for (int i = (*array_len) - 1; i > -1; i--) {
+ int size = *array_len;
+  for (int i = size - 1; i > -1; i--) {
     cout << "date is: " << birthday_list[i].show() << endl;
   }
 
