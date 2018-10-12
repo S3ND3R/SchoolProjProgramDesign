b7ee7d1dee83381941624f5c648a7837de523660
Warren Weber <weber767@umn.edu>
2018-10-12 09:59:45 -0500

    Add(HW01): corrected point2.h point2.cc to match googlestyle guidelines

--- HW/HW01/point2.cc
+++ HW/HW01/point2.cc
@@ -1,18 +1,18 @@
+//  Copyright 2018 Warren Weber
+#include "HW01/point2.h"
+#include <math.h>  //  sqrt, pow
 #include <iostream>
-#include <math.h>	//sqrt, pow
 
-#include "point2.h"
+Point2::Point2() {x_coord = 0.0; y_coord = 0.0;}
 
-Point2::Point2(){x_coord = 0.0; y_coord = 0.0;}
+Point2::Point2(float x, float y) {x_coord = x; y_coord = y;}
 
-Point2::Point2(float x, float y){x_coord = x; y_coord = y;}
+float Point2::DistanceBetween(Point2 point) {
+  //  uses the distance fromula to calculate the distance
+  return sqrt( pow((x_coord - point.get_x_coord()), 2.0)
+             + pow((y_coord - point.get_y_coord()), 2.0));
+}
 
-float Point2::DistanceBetween(Point2 point){
-	// uses the distance fromula to calculate the distance
-	return sqrt( pow((x_coord - point.get_x_coord()), 2.0) + 
-				pow((y_coord - point.get_y_coord()), 2.0));
-	}
+float Point2::get_x_coord() {return x_coord;}
 
-float Point2::get_x_coord(){return x_coord;}
-
-float Point2::get_y_coord(){return y_coord;}
+float Point2::get_y_coord() {return y_coord;}
--- HW/HW01/point2.h
+++ HW/HW01/point2.h
@@ -1,24 +1,24 @@
+"Copyright 2018 Warren Weber"
 
-#ifndef Point2_h
-#define Point2_h
+#ifndef HW01_POINT2_H_
+#define HW01_POINT2_H_
 
 class Point2 {
+ public:
+    /* Constructer for generic point */
+    Point2();
+    /* Constructer for point with passed coordinates */
+    Point2(float x, float y);
+    /* Finds the distance between this point and a passed point */
+    float DistanceBetween(Point2 point);
+    /* get the x coordinate */
+    float get_x_coord();
+    /* get the y coordinate */
+    float get_y_coord();
 
-	public:
-		/*Constructer for generic point*/
-		Point2();
-		/*Constructer for point with passed coordinates*/
-		Point2(float x, float y);
-		/*Finds the distance between this point and a passed point*/
-		float DistanceBetween(Point2 point);
-		/*get the x coordinate*/
-		float get_x_coord();
-		/*get the y coordinate*/
-		float get_y_coord();
-
-	private:
-		float x_coord;
-		float y_coord;
+ private:
+    float x_coord;
+    float y_coord;
 };
 
-#endif //Point2
+#endif  // HW01_POINT2_H_
