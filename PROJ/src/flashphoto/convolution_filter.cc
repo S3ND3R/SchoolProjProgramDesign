8285b71340a8ed2a78898767f1f01256312c938e
Warren Weber <weber767@umn.edu>
2018-11-08 11:04:29 -0600

    access(proj): pushing to access on other machine

--- PROJ/src/flashphoto/convolution_filter.cc
+++ PROJ/src/flashphoto/convolution_filter.cc
@@ -12,47 +12,30 @@ Original Author(s) of this File:
 namespace image_tools {
  ColorData ConvolutionFilter::CalculateFilteredPixel(const PixelBuffer &buffer,
                                                      int x, int y) {
-  // ColorData pixel = buffer.pixel(x,y);
+
   int middle = (kernel_->width() - 1) / 2;
   double red = 0.0;
   double green = 0.0;
   double blue = 0.0;
-  // this loop structure may be wrong
-  // for (int filterY = 0; filterY < kernel_->height(); filterY++) {
-  //  for (int filterX = 0; filterX < kernel_->width(); filterX++) {
-  //   // Checking the pixel is not out of range
-  //   if ((x + (filterX - middle) < 0 ||
-  //   // buffer not
-  //        x + (filterX - middle) > kernel_->width() - 1) ||
-  //       (y + (filterY - middle) < 0 ||
-  //        y + (filterY - middle) > kernel_->height() - 1)) {
-  //    kernel_->set_value(filterY, filterX, 0.0);
-  //   }
-  //  }
-  // }
-  // kernel_->Normalize();
   for (int filterY = 0; filterY < kernel_->height(); filterY++) {
    for (int filterX = 0; filterX < kernel_->width(); filterX++) {
      int pixel_loc_x = x + (filterX - middle);
      int pixel_loc_y = y + (filterY - middle);
      if ((pixel_loc_x >= 0 ||
-          pixel_loc_x <= buffer.height() - 1) ||
+          pixel_loc_x <= buffer.width() - 1) ||
          (pixel_loc_y >= 0 ||
-          pixel_loc_y <= buffer.width() - 1)) {
-      ColorData filter_pixel = buffer.pixel(pixel_loc_x,pixel_loc_y);
+          pixel_loc_y <= buffer.height() - 1)) {
+      // error occurs here
+      ColorData filter_pixel = buffer.pixel(pixel_loc_x, pixel_loc_y);
       red += filter_pixel.red() * kernel_->value(filterY, filterX);
       green += filter_pixel.green() * kernel_->value(filterY, filterX);
       blue += filter_pixel.blue() * kernel_->value(filterY, filterX);
     }
    }
   }
-  //pixel.set_red(red);
-  //pixel.set_green(green);
-  //pixel.set_blue(blue);
   ColorData result = ColorData(red,green,blue);
   result.Clamp();
   return result;
-  // return pixel;
  }
 
  void ConvolutionFilter::SetupFilter() {
--- PROJ/src/flashphoto/tool_blur.cc
+++ PROJ/src/flashphoto/tool_blur.cc
@@ -17,12 +17,15 @@ Author(s) of Significant Updates/Modifications to the File:
 #include "flashphoto/tool_blur.h"
 #include "flashphoto/mask_factory.h"
 
+
 namespace image_tools {
 
 ToolBlur::ToolBlur() {
   // TODO: Students, setup your blur filter to use a radius of 5.0 here
-  blur_filter_->set_kernel_radius(5);
-  blur_filter_->CreateKernel();
+  std::cout << "about to set kernel_radius" << std::endl;;
+  blur_filter_.set_kernel_radius(5);
+  std::cout << blur_filter_.get_kernel_radius() << std::endl;;
+  blur_filter_.CreateKernel();
 
   // the blur operation is not fast, so space the repeated applications of the
   // stamp out as far as we can get away with while still having it look good
@@ -42,8 +45,10 @@ ColorData ToolBlur::LookupPaintColor(int x, int y) {
   // TODO: Students, here's your hook to calculate a filtered version of the
   // pixel. Use your filter to compute the blurred version of the pixel at (x,y)
   // in *buffer_ and return the new color to make this tool work.
-  ColorData filter_color = blur_filter_->CalculateFilteredPixel(*buffer_,
+  std::cout << "Blur about to use CFP" << std::endl;;
+  ColorData filter_color = blur_filter_.CalculateFilteredPixel(*buffer_,
                                                                 x, y);
+  std::cout << "Blur out of to use CFP" << std::endl;;
   // Remove this:  As a placeholder, we're just returning the original pixel
   // color for now.
   return filter_color;
