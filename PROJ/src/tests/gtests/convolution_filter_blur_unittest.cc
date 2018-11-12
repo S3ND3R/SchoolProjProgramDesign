df9afd709df846ce22b09508b163dce40551005e
Warren Weber <weber767@umn.edu>
2018-11-12 09:45:06 -0600

    Add(Proj): added new unnittests and an updated UML class diagram

---
+++ PROJ/src/tests/gtests/convolution_filter_blur_unittest.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2018, Warren Weber, University of Minnesota.
+
+#include <iostream>
+#include "gtest/gtest.h"
+#include "flashphoto/convolution_filter_blur.h"
+
+using image_tools::ConvolutionFilterBlur;
+
+class ConvolutionFilterBlurTest : public ::testing::Test {
+  void SetUp() override {}
+
+ protected:
+  ConvolutionFilterBlur blur_filter;
+};
+
+TEST_F(ConvolutionFilterBlurTest, SetsCorrectKernelRadius) {
+  blur_filter.set_kernel_radius(3);
+
+  EXPECT_EQ(blur_filter.get_kernel_radius(), 3);
+}
---
+++ PROJ/src/tests/gtests/filter_threshold_unittest.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2018, Warren Weber, University of Minnesota.
+
+#include <iostream>
+#include "gtest/gtest.h"
+#include "flashphoto/filter_threshold.h"
+
+using image_tools::FilterThreshold;
+
+class FilterThresholdTest : public ::testing::Test {
+  void SetUp() override {}
+
+ protected:
+  FilterThreshold threshold_filter;
+};
+
+TEST_F(FilterThresholdTest, SetsCorrectThreshold) {
+  threshold_filter.set_threshold(1.0);
+
+  EXPECT_EQ(threshold_filter.threshold(), 1.0);
+}
