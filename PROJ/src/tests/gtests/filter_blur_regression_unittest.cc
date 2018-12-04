496286ef846df03d7115496184970eecba5bf98f
Warren Weber <weber767@umn.edu>
2018-12-03 19:40:53 -0600

    - Added blur filter regression test

--- PROJ/src/tests/gtests/filter_blur_regression_unittest.cc
+++ PROJ/src/tests/gtests/filter_blur_regression_unittest.cc
@@ -1,39 +1,40 @@
-// Copyright (c) 2018, Warren Weber, University of Minnesota.
-//
-// #include <iostream>
-// #include <string>
-// #include "gtest/gtest.h"
-// #include "imagetools/image_editor.h"
-// #include "imagetools/pixel_buffer.h"
-// #include "imagetools/color_data.h"
-//
-// //plan:
-// // load from PROJ ./resources/test_in.png into pixel buffer
-// // apply the test feature
-// // load from PROJ ./resources/blur_5.0_expected.png into a second pixel buffer
-// using image_tools::ConvolutionFilterBlur;
-// using image_tools::ImageEditor;
-// using image_tools::PixelBuffer;
-//
-// class FilterBlurRegressionTest : public ::testing::Test {
-//   void SetUp() override {
-//     test_buffer_.loadFromFile(test_file);
-//     expected_buffer_.loadFromFile(expect_file);
-//     image_editor_.set_pixel_buffer(test_buffer_ptr_);
-//     radius_ = 5.0;
-//   }
-//
-//  protected:
-//   std::string test_file = "./resources/test.png";
-//   std::string expect_file = "./resources/blur_5.0_expected.png";
-//   PixelBuffer test_buffer_(1, 1, image_tools::ColorData(1, 1, 1));
-//   PixelBuffer expected_buffer_(1, 1, image_tools::ColorData(1, 1, 1));
-//   ImageEditor image_editor_;
-//   PixelBuffer *test_buffer_ptr_ = &test_buffer_;
-//   float radius_;
-// };
-//
-// TEST_F(FilterBlurRegressionTest, ImageRegressionEquality) {
-//   image_editor_.ApplyBlurFilter(radius_);
-//   EXPECT_EQ(test_buffer_, expected_buffer_);
-// }
+//Copyright (c) 2018, Warren Weber, University of Minnesota.
+
+#include <iostream>
+#include <string>
+#include "gtest/gtest.h"
+#include "imagetools/image_editor.h"
+#include "imagetools/pixel_buffer.h"
+#include "imagetools/color_data.h"
+
+//plan:
+// load from PROJ ./resources/test_in.png into pixel buffer
+// apply the test feature
+// load from PROJ ./resources/blur_5.0_expected.png into a second pixel buffer
+using image_tools::ConvolutionFilterBlur;
+using image_tools::ImageEditor;
+using image_tools::PixelBuffer;
+
+class FilterBlurRegressionTest : public ::testing::Test {
+  void SetUp() override {
+    //test_buffer_.loadFromFile(test_file);
+    //expected_buffer_.loadFromFile(expect_file);
+    image_editor_.set_pixel_buffer(test_buffer_);
+    radius_ = 5.0;
+  }
+
+ protected:
+  std::string test_file = "./resources/test.png";
+  std::string expect_file = "./resources/blur_5.0_expected.png";
+  PixelBuffer *test_buffer_  = new PixelBuffer(test_file);
+  PixelBuffer *expected_buffer_ = new PixelBuffer(expect_file);
+  ImageEditor image_editor_;
+  float radius_;
+};
+
+TEST_F(FilterBlurRegressionTest, ImageRegressionEquality) {
+  image_editor_.ApplyBlurFilter(radius_);
+  EXPECT_TRUE(*expected_buffer_ == *test_buffer_);
+  //delete test_buffer_;
+  //delete expected_buffer_;
+}
